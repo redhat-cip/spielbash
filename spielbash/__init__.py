0a6d8ca7dc40b22db4f6ebaa421d7e3e6c154831
mhuin <mhuin@redhat.com>
2016-07-01 10:46:18 +0200

    Add copyright info
    Change-Id: Ic9c2d6401c17cf6e345ef13b3604ca4feb1ef2cc

--- spielbash/__init__.py
+++ spielbash/__init__.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python
+#
+# Copyright (C) 2016 Red Hat
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+# -*- coding: utf-8 -*-
 
 import argparse
 import json
@@ -163,7 +178,6 @@ class Movie:
         if self.script.get('title'):
             asciinema_cmd += ' -t %s' % pipes.quote(self.script.get('title'))
         asciinema_cmd += ' %s'
-        print asciinema_cmd % (self.session_name, self.output_file)
         movie = subprocess.Popen(asciinema_cmd % (self.session_name,
                                                   self.output_file),
                                  stdout=subprocess.PIPE,
